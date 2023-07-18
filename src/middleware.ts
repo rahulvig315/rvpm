76bb82e51498d2d10c60a55ca8d75475256ffa7b
Rahul Vig <rahulvig315@gmail.com>
2023-07-17 22:37:13 -0400

    refactor(middleware): update redirect

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,10 +3,10 @@ import { NextRequest, NextResponse } from "next/server";
 export { default } from "next-auth/middleware";
 
 export function middleware(request: NextRequest) {
-    return NextResponse.redirect(new URL('/landing', request.url))
+    return NextResponse.redirect(new URL('/', request.url))
 }
 
 
 export const config = {
-    matcher: ["/((?!landing|api).*)"],
+    matcher: ["/((?!|api).*)"],
 };
