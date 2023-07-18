416aea005e2d9b5f99beadea005c6e6180046d3f
Rahul Vig <rahulvig315@gmail.com>
2023-07-17 22:36:12 -0400

    chore(config): rm unnecessary config

--- next.config.js
+++ next.config.js
@@ -1,13 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-        appDir: true,
-        serverComponentsExternalPackages: ["@prisma/client", "bcryptjs"]
-    },
-    webpack(config) {
-        config.infrastructureLogging = { debug: /PackFileCache/ }
-        return config;
-    }
 }
 
 module.exports = nextConfig
