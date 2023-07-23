d564822802d959d41020a6724be8b8810e24bc4c
Rahul Vig <rahulvig315@gmail.com>
2023-07-23 15:59:44 -0400

    refactor(notification): component bg-color DRY

--- src/app/components/notification/Notification.tsx
+++ src/app/components/notification/Notification.tsx
@@ -1,9 +1,10 @@
-import { Dispatch } from 'react';
+import { AddNotification } from '@/app/hooks/notification';
+import { Dispatch, useMemo } from 'react';
 
 export type NotificationProps = {
     id?: string;
     content?: string;
-    type?: string;
+    type?: AddNotification['type'];
     dispatch?: Dispatch<{
         type?: string;
         notification?: NotificationProps;
@@ -12,8 +13,20 @@ export type NotificationProps = {
 }
 
 export const Notification = ({ id, content, type, dispatch }: NotificationProps) => {
+    const backgroundColor = useMemo(() => {
+        return (type: AddNotification['type']) => {
+            switch (type) {
+                case 'info':
+                    return 'bg-blue-500'
+                case 'success':
+                    return 'bg-green-500'
+                case 'error':
+                    return 'bg-red-500'
+            }
+        }
+    }, [])
     return (
-        <div className={`absolute top-16 left-[50%] m-auto p-2 rounded ${type === 'info' ? 'bg-green-500' : 'bg-red-500'} text-white`}>
+        <div className={`absolute top-16 left-[50%] m-auto p-2 rounded ${backgroundColor} text-white`}>
             {content}
         </div>
     );
