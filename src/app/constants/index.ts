d044554f26d6565ffdf6ca25577656c9d1817a23
Rahul Vig <rahulvig315@gmail.com>
2023-07-23 16:00:14 -0400

    feat(constants): add more constants

--- src/app/constants/index.ts
+++ src/app/constants/index.ts
@@ -4,5 +4,26 @@ const APP_CONSTANTS = {
     APP_DESCRIPTION: 'Simple Kanban CRUD Project Manager'
 }
 
-
 export const { APP_NAME, APP_SHORTNAME, APP_DESCRIPTION } = APP_CONSTANTS;
+
+export const NOTIFICATION_TYPES = {
+    INFO: 'info',
+    SUCCESS: 'success',
+    WARN: 'warn',
+    ERROR: 'error',
+}
+
+export const REQUEST_HEADERS = {
+    CONTENT_TYPE: {
+        "Content-Type": 'application/json'
+    },
+}
+
+export enum RequestMethods {
+    GET = 'GET',
+    POST = 'POST',
+    PUT = 'PUT',
+    PATCH = 'PATCH',
+    OPTIONS = 'OPTIONS',
+    DELETE = 'DELETE'
+}
