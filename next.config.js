71212875060806a50caca1cbd0fde53879ebee86
Rahul Vig <rahulvig315@gmail.com>
2023-07-23 15:57:52 -0400

    fix(config): nxt config redirect fix for signin

--- next.config.js
+++ next.config.js
@@ -1,5 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    async redirects() {
+        return [
+            {
+                source: '/api/auth/signin',
+                has: [
+                    {
+                        type: "query",
+                        key: "error",
+                        value: 'CredentialsSignin'
+                    }
+                ],
+                destination: '/',
+                permanent: true
+            }
+        ]
+    }
 }
 
 module.exports = nextConfig
