081529d5c03cf49d7ae51b358225326baf64c5a6
Rahul Vig <rahulvig315@gmail.com>
2023-07-23 12:57:51 -0400

    feat(buttons): add dashboard setting btn

--- src/app/components/buttons/buttons.tsx
+++ src/app/components/buttons/buttons.tsx
@@ -1,11 +1,20 @@
 'use client'
 
 import { signOut } from "next-auth/react";
+import Link from "next/link";
 
 export const LogoutButton = () => {
     return (
         <button onClick={() => signOut()}>
-            Sign Out
+            Logout
         </button>
+    );
+};
+
+export const SettingsButton = () => {
+    return (
+        <Link href='/dashboard/settings'>
+            Settings
+        </Link>
     );
 };
